2bad99ffefd01b74309aa5f5fce615ba658d2771
duy <tranleduy1233@gmail.com>
2019-06-17 22:04:43 +0700

    Change button label in IDE mode

--- app/src/main/java/com/duy/calculator/symja/activities/IdeActivity.java
+++ app/src/main/java/com/duy/calculator/symja/activities/IdeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 
+import com.duy.calculator.R;
 import com.duy.calculator.activities.base.BaseEvaluatorActivity;
 import com.duy.calculator.evaluator.EvaluateConfig;
 import com.duy.calculator.evaluator.MathEvaluator;
@@ -19,6 +20,7 @@ public class IdeActivity extends BaseEvaluatorActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mBtnEvaluate.setText(R.string.eval);
     }
 
     @Override
