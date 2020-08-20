7546cebcb0e8f8f76e59560b39c2fe9e38aa6d7d
daytoysss <68406836+daytoysss@users.noreply.github.com>
2020-08-20 17:26:44 +0800

    Update Graph2DViewTest.java

--- app/src/test/java/com/duy/ncalc/graph/Graph2DViewTest.java
+++ app/src/test/java/com/duy/ncalc/graph/Graph2DViewTest.java
@@ -8,4 +8,9 @@ public class Graph2DViewTest extends TestCase {
         double a = 1.234566;
         assertEquals((long) (a * 100) / 100d, 1.23);
     }
+    
+    public void testMult() {
+        int b = 5 * 5;
+        assertEquals(25, b);
+    }
 }
